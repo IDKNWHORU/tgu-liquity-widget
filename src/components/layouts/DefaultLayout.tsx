d438e5a57def6f1bd348299d50f5612d3bb0a888
IDKNWHORU <junho9341@gmail.com>
2023-09-21 22:19:31 +0900

    remove navigation

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -10,7 +10,7 @@ interface Props {
 export function DefaultLayout({ children }: Props) {
   return (
     <>
-      <Navigation />
+      {/* <Navigation /> */}
       <BosLoaderBanner />
 
       {children}
